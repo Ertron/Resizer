8243866bfc7ed5edb84e9abce0ddb86043b1a182
ertron <ertron@veldrin.kiev.ua>
2017-05-22 16:46:05 +0300

    show Highlighted size button

--- src/components/social_btns/index.js
+++ src/components/social_btns/index.js
@@ -7,15 +7,14 @@ import { social_objects } from '../../lib/social_buttons';
 @connect(state => state)
 export default class SocialButtons extends Component {
     render() {
-        console.log('this.social_objects', social_objects);
-        let changeSize = function(w, h, that){
+        let changeSize = function(w, h, name, soc_name, that){
             if(w > that.props.file_props.width || h > that.props.file_props.height){
                 console.log('bad size');
                 return false;
             }
             that.props.dispatch({
-                type: 'SOCIAL_SIZE',
-                size: {w: w, h: h}
+                type: 'SOCIAL',
+                params: {w: w, h: h, soc_name: soc_name, name: name}
             });
             that.props.dispatch({
                 type: 'SET_CALCULATED_SIZES',
@@ -27,13 +26,18 @@ export default class SocialButtons extends Component {
                 }
             });
         };
-        let buttonStyle = function (w, h, that) {
+        let buttonStyle = function (w, h, that, btn, btn_type_name, btn_soc_name, selected_soc_name) {
             let color = 'green';
+            let border = '1px solid transparent';
+            if(btn === btn_type_name && btn_soc_name === selected_soc_name){
+                border = '1px solid grey';
+            }
             if(w > that.props.file_props.width || h > that.props.file_props.height){
                 color = 'red';
             }
             return{
-                color: color
+                color: color,
+                border: border
             }
         };
         let that = this;
@@ -43,7 +47,8 @@ export default class SocialButtons extends Component {
                     social_objects.map(function(ub) {
                         let buttons = ub.templates.map(function(button) {
                             return (
-                                <p style={buttonStyle(button.width, button.height, that)} onClick={(e)=> changeSize(button.width, button.height, that)}>{button.type}</p>
+                                <p style={buttonStyle(button.width, button.height, that, button.type, that.props.social.name, ub.name, that.props.social.soc_name)}
+                                   onClick={(e)=> changeSize(button.width, button.height, button.type, ub.name, that)}>{button.type}</p>
                             )
                         });
 
